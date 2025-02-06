8ee18c8485dcf7af8bb546445da7a6f35f9b0826
Nischal Chandur <chandur.nischal@gmail.com>
2025-02-05 20:15:40 -0500

    time series analysis

--- time_series.py
+++ time_series.py
@@ -1,24 +1,50 @@
 from utils.utils import *
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
-team_id = 9
-feature = "boundary_idx"
+def get_team_data(id:int, feature:str) -> pd.DataFrame:
+    query = '''
+    SELECT 
+        date,
+        CASE
+            WHEN team_1_id = {} THEN team_1
+            WHEN team_2_id = {} THEN team_2
+        END as team,
+        CASE 
+            WHEN team_1_id = {} THEN team_1_{}
+            WHEN team_2_id = {} THEN team_2_{}
+        END AS feature
+    FROM elo_ratings
+    WHERE (team_1_id = {} OR team_2_id = {})
+    AND season = 2024
+    ORDER BY date;
+    '''.format(id, id, id, feature, id, feature, id, id)
 
-query = '''
-SELECT 
-    date, 
-    CASE 
-        WHEN team_1_id = {} THEN team_1_{}
-        WHEN team_2_id = {} THEN team_2_{}
-    END AS feature
-FROM elo_ratings
-WHERE (team_1_id = {} OR team_2_id = {}) AND season = 2024
-ORDER BY date;
-'''.format(team_id, feature, team_id, feature, team_id, team_id)
+    data = retrieveFromDB(query=query)
+    data["date"] = pd.to_datetime(data["date"])
 
-data = retrieveFromDB(query=query)
-data["date"] = pd.to_datetime(data["date"])
+    return data
 
-fig = px.scatter(data, x="date", y="feature")
+team_ids = [9, 15, 6, 7, 5]
+feature = "econ"
+
+teams = [get_team_data(id, feature=feature) for id in team_ids]
+
+fig = go.Figure()
+
+for idx, data in enumerate(teams):
+    fig.add_trace(go.Scatter(
+        x=data["date"],
+        y=data["feature"],
+        mode='lines',
+        name=data["team"].iloc[0]
+    ))
+
+fig.update_layout(
+    title="{} Over Time".format(feature),
+    xaxis_title="Date",
+    yaxis_title=feature,
+    showlegend=True
+)
+
 fig.show()
