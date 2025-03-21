8157fbe198081294a71dbff98461783de8440630
nchandur <chandur.nischal@gmail.com>
2025-03-20 20:18:32 -0400

    made prediction path

--- app/face/views.py
+++ app/face/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+sys.path.append(project_root)
+
+from models.predict import predict_game
+
 
 def home(response):
     return render(response, "home.html")
@@ -13,5 +21,8 @@ def match(response):
     if response.method == "POST":
         team1 = response.POST.get("team_1", "")
         team2 = response.POST.get("team_2", "")
-        return render(response, "match.html", {"team_1": team1, "team_2": team2})   
-    return render(response, "match.html", {'data': None})
+
+        output = predict_game([team1, team2])
+    
+        return render(response, "match.html", {"output": output})   
+    return render(response, "match.html", {})
--- models/predict.py
+++ models/predict.py
@@ -1,6 +1,11 @@
 import xgboost
 import pandas as pd
+import numpy as np
 from utils.utils import retrieveFromDB
+
+def get_id(team_name):
+    data = retrieveFromDB(query="SELECT team_id, team_name FROM teams WHERE team_name ILIKE '%%{}%%'".format(team_name))
+    return data.iloc[0].transpose().to_dict()
 
 def get_info(id:int, feature:str) -> float:
     query = '''
@@ -19,14 +24,14 @@ def get_info(id:int, feature:str) -> float:
 
     return data.values[0][0]
 
-def get_latest_info(teams:list) -> pd.DataFrame:
+def get_latest_info(ids:list) -> pd.DataFrame:
     features = ['h2h_win_pct', 'boundary_inv_idx', 'cumulative_boundaries_scored', 'nrr', 'balls', 'h2h_stadium_win_pct', 'cumulative_balls_faced', 'cumulative_wickets_taken', 'boundaries_scored_per_over', 'cumulative_boundaries_conceded', 'dots', 'elo', 'cumulative_balls_bowled', 'wicket_inv_idx', 'wickets', 'cumulative_runs_conceded', 'win_pct', 'boundaries_conceded_per_over', 'wicket_idx', 'pts', 'cumulative_runs_scored', 'cumulative_matches_played', 'extras', 'total', 'rr', 'econ', 'rwb_idx', 'boundary_idx', 'cumulative_wickets_fallen', 'stadium_win_pct', 'boundaries']
 
     instance = {"team_1": {}, "team_2": {}}
 
     for feature in features:
-        instance['team_1'].update({"team_1_{}".format(feature): get_info(id=teams[0], feature=feature)})
-        instance['team_2'].update({"team_2_{}".format(feature): get_info(id=teams[1], feature=feature)})
+        instance['team_1'].update({"team_1_{}".format(feature): get_info(id=ids[0], feature=feature)})
+        instance['team_2'].update({"team_2_{}".format(feature): get_info(id=ids[1], feature=feature)})
 
     team_1 = pd.DataFrame([instance["team_1"]])
     team_2 = pd.DataFrame([instance["team_2"]])
@@ -36,16 +41,24 @@ def get_latest_info(teams:list) -> pd.DataFrame:
 
 
 def predict_game(teams: list):
-    instance = get_latest_info(teams=teams)
 
-    model = xgboost.XGBClassifier()
-    model.load_model("data/weights.json")
+    try:
+        teams = [get_id(name) for name in teams]
+
+        instance = get_latest_info(ids=[teams[0]["team_id"], teams[1]["team_id"]])
+
+        model = xgboost.XGBClassifier()
+        model.load_model("data/weights.json")
+        
+        features = model.get_booster().feature_names
+        instance = instance[features]
+
+        prediction = model.predict_proba(instance)
+
+        return {
+            teams[0]["team_name"]: np.round(prediction[0][0], 3),
+            teams[1]["team_name"]: np.round(prediction[0][1], 3) 
+        }
     
-    features = model.get_booster().feature_names
-    instance = instance[features]
-
-    prediction = model.predict_proba(instance)
-
-    return prediction[0]
-
-print(predict_game(teams=[13, 15]))
+    except:
+        return None
